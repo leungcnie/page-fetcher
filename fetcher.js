251731512fc1306473a50cd18e02a776fe0363dc
Connie <leungcnie@gmail.com>
2020-11-05 02:24:08 +0000

    Added functionality that warns user of overwriting an existing file, and only overwrites if user inputs Y

--- fetcher.js
+++ fetcher.js
@@ -2,6 +2,12 @@ const URL = process.argv[2];
 const PATH = process.argv[3];
 const request = require('request');
 const fs = require("fs");
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 
 
 request(URL, (error, response, body) => {
@@ -9,18 +15,24 @@ request(URL, (error, response, body) => {
 });
 
 const downloadFile = (data) => {
-  fs.writeFile(PATH, data, (err) => {
-    if (err) {
-      console.log(err);
-    } else {
-      printSize(PATH);
-    }
-  })
+  if (fs.existsSync(PATH)) {
+    rl.question("The file already exists. Overwrite file? (Y)  ", (answer) => {
+      if (answer.toUpperCase() === 'Y') {
+        rl.close();
+        fs.writeFile(PATH, data, (err) => printSize(PATH));
+      } else {
+        rl.close();
+      }
+    });
+  } else {
+    r1.close();
+    fs.writeFile(PATH, data, (err) => printSize(PATH));
+  }
 }
 
 const printSize = (file) => {
   const stats = fs.statSync(file);
   const fileSizeInBytes = stats.size;
-  console.log(`Downloaded and saved ${fileSizeInBytes} bytes to ${PATH}`);
+  return console.log(`Downloaded and saved ${fileSizeInBytes} bytes to ${PATH}`);
 }
 
